aeef711ff740694935634a6d2b3030757ecb3dc6
Lashen Martino <lashenmartino@gmail.com>
2024-07-04 16:06:09 +0530

    Implemented Add Member operation

--- src/main/java/org/example/util/DBConnection.java
+++ src/main/java/org/example/util/DBConnection.java
@@ -10,7 +10,7 @@ public class DBConnection {
 
     private DBConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/library_test", "root", "Silm_220721227");
+        connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/library_test", "root", "7889");
     }
 
 
