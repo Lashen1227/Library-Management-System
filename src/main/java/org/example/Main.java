ce42e20c805d347223b7ea7a63a9aeed21bf8336
Lashen Martino <lashenmartino@gmail.com>
2024-07-06 14:22:35 +0530

    Develop System UI

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         AppInitializer.main(args);
-        System.out.println("Welcome to the Library Management System!");
+        System.out.println("Thank You For Using Our System.");
     }
 }
