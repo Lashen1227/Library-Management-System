9688def1d02be8c41b56de35258939d1c110cd9d
Lashen Martino <lashenmartino@gmail.com>
2024-07-10 12:44:12 +0530

    Update System UI

--- src/main/java/org/example/controller/MainDashboardFormController.java
+++ src/main/java/org/example/controller/MainDashboardFormController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Alert;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
@@ -43,4 +44,7 @@ public class MainDashboardFormController {
         }
     }
 
+    public void btnBackOnAction(MouseEvent mouseEvent) {
+        mainPane.getScene().getWindow().hide();
+    }
 }
